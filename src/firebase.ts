bec28665fdc88378b0425323b94a8821924a070f
Huba <hieu.dcu@gmail.com>
2024-08-16 03:32:36 +1200

    Removed .env file

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,10 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBVdBTrmXre9h2KbgjvkEMoWR0MLki0Q_I",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
