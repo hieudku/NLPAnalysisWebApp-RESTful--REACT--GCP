17cc58d9f051b3887e576cb53f0fd3a82c6b3c64
Huba <hieu.dcu@gmail.com>
2024-08-16 21:39:34 +1200

    Integrate natural language API.

---
+++ functions/src/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import * as functions from 'firebase-functions';
+import * as language from '@google-cloud/language';
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const client = new language.LanguageServiceClient();
