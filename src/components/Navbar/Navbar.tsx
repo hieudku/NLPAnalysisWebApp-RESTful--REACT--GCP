09a4f7e5486e44c1f9ff4b885bfdb4c2e406ac78
Huba <hieu.dcu@gmail.com>
2024-08-25 02:49:47 +1200

    Logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const Navbar:React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -44,9 +45,17 @@ const Navbar:React.FC = () => {
                 </svg>
             </button>
 
-            <div className="navbar-brand">v0.5.5</div>
+            <div className="navbar-brand">
+                <div className="brand-logo">
+                <p><strong>Unredacted</strong></p>
+                <VisibilityIcon sx={{ fontSize: 30, color: '#f1f1f1' }}/>
+                </div>
+            </div>
 
             <div ref={panelRef} className={`navbar-panel ${isOpen ? 'open' : ''}`}>
+                <img src="/logo3.png" alt="App Logo" />
+                <br />
+                <br />
                 <a className= "navbar-link" href="/login">Login</a>
                 <a className= "navbar-link" href="/register">Register</a>
                 <br />
